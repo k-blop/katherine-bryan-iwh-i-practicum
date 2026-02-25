a48aa7432bb6053bac5bc9190e9853762e4020ba
Katherine Bryan <katherinebryan@MacBookAir.home>
2026-02-25 12:32:31 +0100

    Added Express routes and Pug templates

--- index.js
+++ index.js
@@ -7,65 +7,68 @@ app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+// * Please DO NOT INCLUDE the private app access token in your repo. Don't do this either here or anywhere else.
+// * Use a .env file instead.
+const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 
-// TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+// IMPORTANT: Replace this with your actual custom object ID or type name (e.g., 'coffee' or 'p1234567_coffee')
+// If using a standard object, just use 'contacts'
+const OBJECT_TYPE = 'coffee'; 
 
-// * Code for Route 1 goes here
-
-// TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
-
-// TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
-
-// * Code for Route 3 goes here
-
-/** 
-* * This is sample code to give you a reference for how you should structure your calls. 
-
-* * App.get sample
-app.get('/contacts', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
-    const headers = {
-        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
-        'Content-Type': 'application/json'
-    }
-    try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('contacts', { title: 'Contacts | HubSpot APIs', data });      
-    } catch (error) {
-        console.error(error);
-    }
-});
-
-* * App.post sample
-app.post('/update', async (req, res) => {
-    const update = {
-        properties: {
-            "favorite_book": req.body.newVal
-        }
-    }
-
-    const email = req.query.email;
-    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+// ROUTE 1: GET Homepage
+app.get('/', async (req, res) => {
+    // Get all custom properties we need
+    const customObjectRoute = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}?properties=name,info,bean_type`;
+    
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     };
 
-    try { 
-        await axios.patch(updateContact, update, { headers } );
-        res.redirect('back');
-    } catch(err) {
-        console.error(err);
+    try {
+        const response = await axios.get(customObjectRoute, { headers });
+        const records = response.data.results;
+        
+        // Pass the data to the pug template
+        res.render('homepage', { title: 'Custom Objects | HubSpot Practicum', records });      
+    } catch (error) {
+        console.error('Error getting objects:', error.message);
+        res.send('There was an error loading the homepage.');
     }
-
 });
-*/
 
+// ROUTE 2: GET Update Form
+app.get('/update-cobj', (req, res) => {
+    res.render('updates', { title: 'Update Custom Object Form | Integrating With HubSpot I Practicum' });
+});
 
-// * Localhost
+// ROUTE 3: POST Update Form
+app.post('/update-cobj', async (req, res) => {
+    const updateRoute = `https://api.hubapi.com/crm/v3/objects/${OBJECT_TYPE}`;
+    
+    // Grab the data from the form
+    const newRecord = {
+        properties: {
+            "name": req.body.name,
+            "bio": req.body.info,
+            "animal_type": req.body.bean_type
+        }
+    };
+
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        await axios.post(updateRoute, newRecord, { headers });
+        // Redirect back to homepage after successful creation
+        res.redirect('/');
+    } catch (error) {
+        console.error('Error creating object:', error.message);
+        res.send('There was an error creating the custom object.');
+    }
+});
+
+// Localhost server
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
